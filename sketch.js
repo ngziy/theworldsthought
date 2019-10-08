4e19ec6f64540c1bc1a43016d82126974f9181af
Guest User <Guest@wg401-i-06-07.mddn.vuw.ac.nz>
2019-10-08 16:08:42 +1300

    Working API

--- sketch.js
+++ sketch.js
@@ -1,84 +1,55 @@
-
-let JSONmessages;
-
+var url = 'https://mddn201project3-zed.firebaseio.com/messages.json';
 let keys;
+let JSONmessages;
 let textPos = [];
-
 let bg
-let scaleRatio = 0
-
-  let url1 = 'https://mddn201project3-zed.firebaseio.com/messages.json';
-
-let timer = 0;
+let buffer = [];
 
 function preload() {
 
-  JSONmessages = loadJSON(url1,"jsonp")
+    bg = loadImage('bg-01.png');
 
-  bg = loadImage('bg-01.png');
- 
 }
 
-
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+    for (let i = 0; i < 20; i++) {
+        textPos[i] = {
+            px: random(-width / 2, width / 2),
+            py: random(50, (height / 4) * 3),
+            speed: random(1, 5)
+        };
+    }
 
-  createCanvas(windowWidth, windowHeight);
-  keys = Object.keys(JSONmessages);
+    setInterval(askISS, 1000);
+}
 
+function askISS() {
+    JSONmessages = loadJSON(url, gotData);
+}
 
-
-  for (let i = 0; i < 20; i++) {
-    textPos[i] = {
-      px: random(-width/2, width/2),
-      py: random(100, (height/4)*3),
-      speed: random(2, 4)
-    };
-  }
-
-  scaleRatio = windowWidth/bg.width
-
-    rectMode(CENTER)
-    textFont("Rubik Mono One");
-    textSize(height/75);
-    textAlign(CENTER, CENTER);
-    
-  }
-
-function draw() {
-
-
-  	background(255);
-    image(bg,((width-bg.width)/2),(windowHeight-bg.height));
-      for(let k=1; k<15 ; k++) {
-        noStroke()
-        fill(0,125)
-        rect(textPos[k].px, textPos[k].py, width/4, height/7)
-        fill(255)
-        text(JSONmessages[keys[keys.length-k]].messages, textPos[k].px, textPos[k].py, width/4, height/7);
-        textPos[k].px+=textPos[k].speed;
-        if(textPos[k].px > width*1.5) {
-          textPos[k].px = -width/.5;
+function gotData(data) {
+    keys = Object.keys(JSONmessages);
+    if (JSONmessages) {
+        for (let k = 1; k < 15; k++) {
+            buffer.push(JSONmessages[keys[keys.length - k]].messages);
         }
-      }
-
-
-
-  timer++;
-  if(timer> 1500) {
-    // JSONmessages = loadJSON(url1,"jsonp")
-    // keys = Object.keys(JSONmessages);
-    location.reload()
-    timer = 0;
-    
-
-
-  }
-  }
-
-
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  location.reload()
-  
+    }
+}
+function draw() {
+    background(255);
+    image(bg, ((windowWidth - bg.width) / 2), (windowHeight - bg.height));
+    if (buffer.length >= 15) {
+        for (let k = 1; k < 15; k++) {
+            noStroke()
+            fill(0, 125)
+            rect(textPos[k].px, textPos[k].py, width / 4, height / 7)
+            fill(255)
+            text(buffer[buffer.length-k], textPos[k].px, textPos[k].py, width/4, height/7);
+            textPos[k].px += textPos[k].speed;
+            if (textPos[k].px > width * 2) {
+                textPos[k].px = -width / 2;
+            }
+        }
+    }
 }
